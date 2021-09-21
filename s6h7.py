ef7d1797ed9c38d88c6350bca12405dcbf57c48f
Tahereh Mohammadi <88844055+TaherehMohammadi@users.noreply.github.com>
2021-09-21 14:47:09 +0430

    Delete s6h7.py

--- s6h7.py
+++
@@ -1,8 +0,0 @@
-# Tahereh Mohammadi - Thursday 14-18
-# write every words in the list in vertical by for
-
-names=['ali','reza','sara','hadi','negar','mina']
-for i,name in enumerate(names):
-        for char in name:
-            print(char)
-        print('-------')
