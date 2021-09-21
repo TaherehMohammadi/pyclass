7c9fb3725603b9261efe6081296d32cce9bf1820
Tahereh Mohammadi <88844055+TaherehMohammadi@users.noreply.github.com>
2021-09-21 14:48:19 +0430

    Delete s6h5.py

--- s6h5.py
+++
@@ -1,9 +0,0 @@
-# Tahereh Mohammadi - Thursday 14-18
-# say the number of the every person in the list by while
-
-people=['ashkan','mahdi','atefeh','amirabbas']
-n=0
-while n<len(people):
-    name=people[n]
-    print(f'{name} is the person {n+1} in the list.')
-    n=n+1
