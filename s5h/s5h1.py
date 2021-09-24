c2a24497cce659cd245b62923cb99f68d81c355f
Tahereh Mohammadi <88844055+TaherehMohammadi@users.noreply.github.com>
2021-09-24 18:18:13 +0330

    Delete s5h1.py

--- s5h/s5h1.py
+++
@@ -1,7 +0,0 @@
-# Tahereh Mohammadi - Thursday 14-18
-# Merge 3 lists in one and sort in reverse and one in between
-list1=[1,2,3]
-list2=[4,5,6]
-list3=[7,8,9]
-tlist=list1+list2+list3
-print(tlist[::-2])
