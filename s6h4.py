252e33d58a04743c530afa7c58b10014befabaff
Tahereh Mohammadi <88844055+TaherehMohammadi@users.noreply.github.com>
2021-09-24 18:22:27 +0330

    Delete s6h4.py

--- s6h4.py
+++
@@ -1,8 +0,0 @@
-# Tahereh Mohammadi - Thursday 14-18
-# sum of even numbers between 1000 to 2000 by for
-sum0=0
-for n in range(1000,2000):
-    if n%2 != 0:
-        sum0=sum0+n
-        n=n+1
-print(sum0)
